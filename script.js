788f83f9706022658523a7c19f4a959a02e159e0
jalebi <mehrdad.jalebi@gmail.com>
2022-05-21 11:51:12 +0430

    first task: create database with handers

--- script.js
+++ script.js
@@ -1,9 +1,51 @@
 const MONTHS = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-const months = document.getElementById('month')
-MONTHS.forEach(month => {
+const taskList = document.getElementById('task-list');
+const title = document.getElementById('title');
+const year = document.getElementById('year');
+const month = document.getElementById('month')
+const day = document.getElementById('day');
+const hour = document.getElementById('hour');
+const min = document.getElementById('min');
+const submit = document.getElementById('submit');
+const notifBtn = document.getElementById('enable');
+
+MONTHS.forEach(m => {
   var opt = document.createElement('option');
-    opt.value = month;
-    opt.innerHTML = month;
-    months.appendChild(opt);
+    opt.value = m;
+    opt.innerHTML = m;
+    month.appendChild(opt);
 })
 
+let db;
+
+let newTask = [
+  { 
+    title: "",
+    hours: 0,
+    minutes: 0,
+    day: 0,
+    month: "",
+    year: 0,
+    notified: "no"
+  }
+];
+
+if(Notification.permission === 'denied' || Notification.permission === 'default') {
+  notifBtn.style.display = 'block';
+} else {
+  notifBtn.style.display = 'none';
+}
+
+window.onload = function() {
+  /**** first task *****/
+  // create a "toDoList" database in indexeddb
+  const DBOpenRequest = window.indexedDB.open("toDoList", 1);
+  DBOpenRequest.onerror = event => {
+    console.error(event)
+  };
+  DBOpenRequest.onsuccess = event => {
+    db = DBOpenRequest.result;
+    //displayData();
+  };
+  /**** first task ended *****/
+}
