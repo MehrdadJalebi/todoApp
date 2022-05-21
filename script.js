3b8a6f21b7abfa977886a07f9beb11e8a2aa4a19
jalebi <mehrdad.jalebi@gmail.com>
2022-05-21 13:48:44 +0430

    feat: create a list

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ const day = document.getElementById('day');
 const hour = document.getElementById('hour');
 const min = document.getElementById('min');
 const submit = document.getElementById('submit');
-const notifBtn = document.getElementById('enable');
 
 MONTHS.forEach(m => {
   var opt = document.createElement('option');
@@ -26,48 +25,37 @@ let newTask = [
     day: 0,
     month: "",
     year: 0,
-    notified: "no"
   }
 ];
 
-if(Notification.permission === 'denied' || Notification.permission === 'default') {
-  notifBtn.style.display = 'block';
-} else {
-  notifBtn.style.display = 'none';
-}
+/**** first task *****/
+// create a "toDoList" database in indexeddb
+const DBOpenRequest = window.indexedDB.open("toDoList", 1);
+DBOpenRequest.onerror = event => {
+  console.error(event)
+};
+DBOpenRequest.onsuccess = event => {
+  console.log(DBOpenRequest.result)
+  db = DBOpenRequest.result;
+  displayData();
+};
+/**** first task ended *****/
 
-
-window.onload = function() {
-  /**** first task *****/
-  // create a "toDoList" database in indexeddb
-  const DBOpenRequest = window.indexedDB.open("toDoList", 1);
-  DBOpenRequest.onerror = event => {
+/**** second task *****/
+// handle versions and making objectstore
+DBOpenRequest.onupgradeneeded = event => {
+  let db = event.target.result;
+  db.onerror = event => {
     console.error(event)
   };
-  DBOpenRequest.onsuccess = event => {
-    console.log(DBOpenRequest.result)
-    db = DBOpenRequest.result;
-    //displayData();
-  };
-  /**** first task ended *****/
-  
-  /**** second task *****/
-  // handle versions and making objectstore
-  DBOpenRequest.onupgradeneeded = event => {
-    let db = event.target.result;
-    db.onerror = event => {
-      console.error(event)
-    };
-    let objectStore = db.createObjectStore("tasks", { keyPath: "title" });
-    objectStore.createIndex("year", "year", { unique: false });
-    objectStore.createIndex("month", "month", { unique: false });
-    objectStore.createIndex("day", "day", { unique: false });
-    objectStore.createIndex("hour", "hour", { unique: false });
-    objectStore.createIndex("min", "min", { unique: false });
-    objectStore.createIndex("notified", "notified", { unique: false });
-  };
-  /**** second task ended *****/
-}
+  let objectStore = db.createObjectStore("tasks", { keyPath: "title" });
+  objectStore.createIndex("year", "year", { unique: false });
+  objectStore.createIndex("month", "month", { unique: false });
+  objectStore.createIndex("day", "day", { unique: false });
+  objectStore.createIndex("hour", "hour", { unique: false });
+  objectStore.createIndex("min", "min", { unique: false });
+};
+/**** second task ended *****/
 
 /**** third task *****/
 // add tasks to objectStore
@@ -79,8 +67,7 @@ function addTask() {
       month: month.value,
       day: day.value,
       hour: hour.value,
-      min: min.value,
-      notified: "no"
+      min: min.value
     }
   ];
   let transaction = db.transaction(["tasks"], "readwrite");
@@ -93,6 +80,32 @@ function addTask() {
     day.value = null;
     hour.value = null;
     min.value = null;
+    displayData();
   };
 }
 /**** third task ended *****/
+
+
+/**** fourth task *****/
+//create a list of tasks
+function displayData() {
+  while (taskList.firstChild) {
+    taskList.removeChild(taskList.lastChild);
+  }
+  let objectStore = db.transaction('tasks').objectStore('tasks');
+  objectStore.openCursor().onsuccess = event => {
+    let cursor = event.target.result;
+    if(cursor) {
+      const toDoText = cursor.value.title;
+      const listItem = createListItem(toDoText);
+      taskList.appendChild(listItem);
+      cursor.continue();
+    }
+  }
+}
+function createListItem(contents) {
+  const listItem = document.createElement('li');
+  listItem.textContent = contents;
+  return listItem;
+}
+/**** fourth task ended *****/
